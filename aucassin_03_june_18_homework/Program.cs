3f83e46e4abf5ce4c895accc11c42bfff2ea1062
Elias Boiko <elias.boiko@gmail.com>
2018-06-03 17:03:05 +0300

    Add project files.

---
+++ aucassin_03_june_18_homework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aucassin_03_june_18_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
